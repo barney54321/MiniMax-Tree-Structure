8e37335f79ab01d7263cbecb8a93ac19fa890501
Andrew Esteban <andrewesteban1999@gmail.com>
2019-09-17 18:29:28 +1000

    created scaffold

---
+++ Scaffold/Minimax.java
@@ -0,0 +1,15 @@
+public class Minimax {
+
+    public Minimax(int[] leaves) {
+
+    }
+
+    public Node getHead() {
+        return null;
+    }
+
+    // Use for own testing purposes
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Scaffold/Node.java
@@ -0,0 +1,6 @@
+public class Node {
+
+    public static int log2(int x) {
+        return (int) (Math.log(x) / Math.log(2));
+    }
+}
